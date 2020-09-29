ac58f38227a240d233fe185804379d9ed2ad2f1d
Ryuichi2516 <shake.hands0011@gmail.com>
2020-09-29 19:20:48 +0900

    Create_device

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: "users/sessions",
+  }
   root 'products#index'
+
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "logout", :to => "users/sessions#destroy"
+  end
 end
