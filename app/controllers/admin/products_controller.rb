e43101ddc462805aa2a36b45c772353b8cde7254
Ryuichi2516 <shake.hands0011@gmail.com>
2020-11-08 22:27:08 +0900

    add_new_products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,37 @@
 class Admin::ProductsController < ApplicationController
+
   def new
+    @product = Product.new
+    @image = @product.images.build
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      if params[:images].present?
+        params[:images][:file].each do |image|
+          @image = @product.images.create!(file: image, product_id:@product.id)
+        end
+        redirect_to root_path and return
+      end
+      render :new and return
+    end
+    render :new
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category_id,
+      :brand_id,
+      :quantity,
+      :price,
+      :stock,
+      images_attributes: [:file]
+    )
   end
 end
--- db/migrate/20201102233922_create_products.rb
+++ db/migrate/20201102233922_create_products.rb
@@ -3,11 +3,11 @@ class CreateProducts < ActiveRecord::Migration[5.2]
     create_table :products do |t|
       t.string :name, null: false
       t.text :description, null: false
-      t.integer :price, null: false
-      t.integer :quantity, null: false
-      t.references :brand, null: false
-      t.boolean :stock, default: false, null: false
       t.references :category
+      t.references :brand, null: false
+      t.integer :quantity, null: false
+      t.integer :price, null: false
+      t.boolean :stock, default: false, null: false
       t.timestamps
     end
   end
