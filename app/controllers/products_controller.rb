2f7ff263469c51dcb7fd95749edf8dfe1128e54b
Ryuichi2516 <shake.hands0011@gmail.com>
2020-11-17 14:19:15 +0900

    modify_css

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    @NewProducts = Product.all.includes(:images).order(created_at: :desc).limit(7)
-    @StockProducts = Product.where(stock:true).includes(:images).order(created_at: :desc).limit(7)
+    @NewProducts = Product.where.not(stock: true).includes(:images).order(created_at: :desc).limit(7)
+    @StockProducts = Product.where(stock: true).includes(:images).order(created_at: :desc).limit(7)
     @RandomProducts = Product.all.includes(:images).order("RAND()").limit(7)
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
 end
